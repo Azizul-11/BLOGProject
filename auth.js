0f6fd11f88f3b75d0e1eeed5cf680f935e3e24b2
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-06-14 18:11:39 +0530

    auth.js
    appwrite backend code for login logout and

---
+++ auth.js
@@ -0,0 +1,38 @@
+import conf from './../conf/conf';
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client.setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        const userAccount = await this.account.create(ID.unique(), email, password, name);
+        if (userAccount) {
+            return this.login({ email, password });
+        } else {
+            return userAccount;
+        }
+    }
+
+    async login({ email, password }) {
+        return await this.account.createEmailSession(email, password);
+    }
+
+    async getCurrentUser() {
+       return await this.account.get();
+    }
+
+    async logout(){
+         await this.account.deleteSessions();
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
