ac37211db436497cfc5b3dcda05fd82f522da4bf
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-06-19 14:12:58 +0530

    Add files via upload

---
+++ Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Container = ({chilldren}) => {
+  return (
+    <div className='w-full max-w-7xl mx-auto px-4 
+    '>{chilldren}</div>
+  )
+}
+
+export default Container
